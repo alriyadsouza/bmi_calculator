2223f40a94378b216a49042a6d9bdf8dff77844a
alriyadsouza <alriyatreeza929@gmail.com>
2023-05-26 23:42:16 +0530

    Initial commit

---
+++ lib/input_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI CALCULATOR'),
+      ),
+      body: const Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          null;
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Color(0xFF0A0E21),
+        ).copyWith(secondary: Colors.teal[300],
+        ),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+          textTheme: const TextTheme(
+            bodyText2: TextStyle(color: Colors.white), // Set black color for the text style
+          ),
+      ),
+      home: InputPage(),
+    );
+  }
+}
+
