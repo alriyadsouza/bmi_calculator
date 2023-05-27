f608e2a6af40c2182ca55dc7ddcef4984e500223
alriyadsouza <alriyatreeza929@gmail.com>
2023-05-27 09:51:10 +0530

    made short

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -11,14 +12,56 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: const Text('BMI CALCULATOR'),
       ),
-      body: const Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(children: [
+              Expanded(
+                child: reusableCard(colour: Color(0xFF1D1E33),),
+              ),
+              Expanded(
+                child: reusableCard(colour: Color(0xFF1D1E33),),
+              ),
+            ]),
+          ),
+           Expanded(
+            child: reusableCard(colour: Color(0xFF1D1E33),),
+          ),
+          Expanded(
+            child: Row(children: [
+              Expanded(
+                child: reusableCard(colour: Color(0xFF1D1E33),),
+              ),
+              Expanded(
+                child: reusableCard(colour: Color(0xFF1D1E33),),
+              ),
+            ]),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           null;
         },
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class reusableCard extends StatelessWidget {
+
+  reusableCard({required this.colour});
+
+  Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
